28bf93b82fe406633dd7dd1f527e7b73ff429d90
rranand <44535350+rranand@users.noreply.github.com>
2019-11-09 19:23:19 +0530

    Update Detect HTML links.py

--- HR/Detect HTML links.py
+++ HR/Detect HTML links.py
@@ -9,5 +9,19 @@ for i in text:
     a=list(re.findall('<a href="(.*?)\"{1}',i))
     b=list(re.findall('<a href=.*?>(.*?)</a>',i))
     if len(a)>0 and len(b)>0:
+        for i in range(len(b)):
+            z=b[i]
+            if bool(re.search('<',b[i]))==True:
+                z=list(re.findall('<.>(.*?)<.*>',z))
+                if len(z)>0:
+                    z=z.pop()
+            if len(z)>0:
+                z=z.lstrip()
+            
+            b[i]=z
         for i in range(len(a)):
-           print(a[i]+','+b[i])
+            z=a[i]+','
+            if b[i]==[]:
+                print(z)
+            else:
+                print(z+str(b[i]))
