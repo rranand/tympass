e08aa8dfdf35c2a66bda5bce0d767bb4bef7378d
Rohit Anand <rrohitanand3336@gmail.com>
2020-04-23 03:07:38 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -6,8 +6,12 @@ int main() {
   cout << "Enter total number of cornors: ";
   cin >> vertex;
   int distance[vertex][vertex];
+  int visited[vertex];
+  for(int i=0;i<vertex;i++) {
+    visited[i]=0;
+  }
 
-  cout << "If distance does not exists between two paths as directed then enter 0/n";
+  cout << "If distance does not exists between two paths as directed then enter 0\n";
   for(int i=0; i<vertex;i++) {
     for(int j=0; j<vertex;j++) {
       cout << "Enter distance between " << i+1 << " and " << j+1 << ": ";
@@ -15,21 +19,21 @@ int main() {
     }
   }
   
-  int tempMax =9999, currentV=0;
-  while (vertex>count) {
-
+  int currentV=0;
+  while (vertex-1>count) {
+    int tempMax =9999;
+    visited[currentV] = 1;
     for(int i=0; i<vertex;i++) {
       if (distance[currentV][i]==0) {
         continue;
-      } else {
-        if (minDis+distance[currentV][i]<tempMax) {
-          tempMax = minDis+distance[currentV][i];
-          temp= i;
-        }
       }
-      minDis = tempMax;
-      currentV = temp;
+      if (distance[currentV][i]<tempMax && visited[i]==0) {
+        tempMax = distance[currentV][i];
+        temp= i;
+      }
     }
+    minDis += tempMax;
+    currentV = temp;
     count++;
   }
   cout << "Minimum Distance: " << minDis;
