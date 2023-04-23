65139032223b1ea0ce14ccf934267efb097e9ec8
Rohit Anand <rrohitanand3336@gmail.com>
2023-04-23 23:49:39 +0530

    Update excelAutomation.py

--- excelAutomation.py
+++ excelAutomation.py
@@ -12,7 +12,12 @@ from datetime import date
 #path = input("Enter Path: ")
 path = sys.argv[1]
 
-wbObj = openpyxl.load_workbook(path)
+try:
+    wbObj = openpyxl.load_workbook(path)
+except IOError:
+    print('No File Found')
+    exit(0)
+
 sheet = wbObj[wbObj.active.title]
 
 arr = []
@@ -36,19 +41,34 @@ today = date.today()
 for i in range(2, sheet.max_row+1):
     cellDateTime  =sheet.cell(i, 2).value
     diff = (today - date(cellDateTime.year, cellDateTime.month, cellDateTime.day)).days
+    weekDay = today.weekday
 
-    if diff <= 2:
-        sheet.cell(i, col_mx_value).value = "0-2 days"
-    elif diff <= 4:
-        sheet.cell(i, col_mx_value).value = "2-4 days"
-    elif diff <= 10:
-        sheet.cell(i, col_mx_value).value = "5-10 days"
-    elif diff <= 20:
-        sheet.cell(i, col_mx_value).value = "11-20 days"
-    elif diff <= 30:
-        sheet.cell(i, col_mx_value).value = "21-30 days"
+    if weekDay == 0:
+        if diff <= 5:
+            sheet.cell(i, col_mx_value).value = "0-2 days"
+        elif diff <= 7:
+            sheet.cell(i, col_mx_value).value = "2-4 days"
+        elif diff <= 16:
+            sheet.cell(i, col_mx_value).value = "5-10 days"
+        elif diff <= 26:
+            sheet.cell(i, col_mx_value).value = "11-20 days"
+        elif diff <= 36:
+            sheet.cell(i, col_mx_value).value = "21-30 days"
+        else:
+            sheet.cell(i, col_mx_value).value = "More than 30 days"
     else:
-        sheet.cell(i, col_mx_value).value = "More than 30 days"
+        if diff <= 2:
+            sheet.cell(i, col_mx_value).value = "0-2 days"
+        elif diff <= 4:
+            sheet.cell(i, col_mx_value).value = "2-4 days"
+        elif diff <= 10:
+            sheet.cell(i, col_mx_value).value = "5-10 days"
+        elif diff <= 20:
+            sheet.cell(i, col_mx_value).value = "11-20 days"
+        elif diff <= 30:
+            sheet.cell(i, col_mx_value).value = "21-30 days"
+        else:
+            sheet.cell(i, col_mx_value).value = "More than 30 days"
 
 
 arr.clear()
@@ -63,13 +83,14 @@ for i in arr[0]:
 
 df = pd.DataFrame(arr)
 df.columns = columnNames
+
 df.drop(axis=1, index= 0, inplace=True)
 
 pvTable = df.pivot_table(index='Assignment Group', columns='Aging Days', values='Assigned To', aggfunc='count', margins=True, margins_name='Grand Total')
 
 df['copy_Assigned To'] = df.loc[:, 'Assigned To']
 pvTableAssociate = df.pivot_table(index='Assigned To', columns='Task type', values='copy_Assigned To', aggfunc='count', margins=True, margins_name='Grand Total')
-df = df.drop(columns=['copy_Assigned To'])
+df.drop(columns=['copy_Assigned To'], inplace=True)
 
 fileName = "".join(path.split(".")[:-1])
 
